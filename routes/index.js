7d19882e2e6a63acb86d573efb7644b0036ff9b1
MJGR <mjgriley@gmail.com>
2021-11-09 14:17:46 -0500

    workout model

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+// get api workouts (findAll)
+// get by id / api / workouts (findOne)
+// put / api / workouts
+// post / api / workouts (add entry)
+// get / api / workouts / Range(findAll)
+// route to /exercise to exercise.html
+const router = require('express').Router()
+const Workout = require('../models')
---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const { Mongoose } = require("mongoose")
+
+PORT
+
+

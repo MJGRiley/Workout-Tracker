bf361b10cb1e544354590435862f08ce0ac73c27
MJGR <mjgriley@gmail.com>
2021-11-09 20:27:54 -0500

    I forgot to commit oftem

--- models/workout.js
+++ models/workout.js
@@ -3,36 +3,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  type: {
-    type: String,
-    trim: true,
-    required: "Enter a type of workout"
-  },
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for transaction"
-  },
-  duration: {
-    type: Number,
-    required: "Enter a duration"
-  },
-  distance: {
-      type: Number,
-  },
-  weight: {
-    type: Number,
-  },
-  reps: {
-    type: Number,
-  },
-  sets: {
-    type: Number,
-  },
   day: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: "Enter a workout type",
+      },
+      name: {
+        type: String,
+        required: "Enter a workout name",
+      },
+      duration: {
+        type: Number,
+        required: "Enter a duration",
+      },
+      distance: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
--- server.js
+++ server.js
@@ -1,6 +1,23 @@
-const express = require('express')
-const { Mongoose } = require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
 
-PORT
+const PORT = process.env.PORT || 3000;
 
+const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// routes
+app.use(require("./routes/index"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});

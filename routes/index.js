2d8a75af7feab7bae8f7ca9d9943c403cd83cfcd
MJGR <mjgriley@gmail.com>
2021-11-10 09:53:04 -0500

    heroku probs

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 // static route from /exercise to exercise.html
 const path = require('path')
 const router = require("express").Router();
-const Workout = require("../models/Workout");
+const Workout = require("../models/workout");
 
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/exercise.html'))

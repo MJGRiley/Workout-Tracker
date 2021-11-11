00aabbbfaf32d56798a4368bba3311288b0a5c45
MJGR <mjgriley@gmail.com>
2021-11-11 17:34:20 -0500

    heroku seed

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
     {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false
 });
 
 // routes
